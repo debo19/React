42067230b4cccec38cba53400de149b5b554399b
Debopriya Bhattacharjee <debopriyabh1908@gmail.com>
2021-08-29 01:35:08 +0530

    added files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Setup from './tutorial/1-useState/2-useState-basics'
+function App() {
+  return (
+    <div className='container'>
+      <Setup />
+    </div>
+  )
+}
+
+export default App
